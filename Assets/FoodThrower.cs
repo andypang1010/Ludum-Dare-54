4580bb27b9fdf32b242a86ab1a0fa49a253d4cc6
AW0723 <64997746+AW0723@users.noreply.github.com>
2023-09-30 09:34:39 -0500

    Food thrower

--- Assets/FoodThrower.cs
+++ Assets/FoodThrower.cs
@@ -1,39 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FoodThrower : MonoBehaviour
 {
     public GameObject food;
+    public float speed = 15;
     public float throwTimeInterval = 2;
     private float lastThrowTime = 0;
     private float spawnRadius = 10f;
-    private float speed;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        if(Time.time >= lastThrowTime + throwTimeInterval) {
+        if (Time.time >= lastThrowTime + throwTimeInterval)
+        {
             ThrowFood();
             lastThrowTime = Time.time;
         }
     }
 
-    private void ThrowFood() {
-        float angle = Random.Range(0, 2*Mathf.PI);
-        Vector2 spawnPos = new Vector2(Mathf.Cos(angle),Mathf.Sin(angle)) * spawnRadius;
+    private void ThrowFood()
+    {
+        float angle = Random.Range(0, 2 * Mathf.PI);
+        Vector2 spawnPos = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * spawnRadius;
         GameObject foodObj = Instantiate(food, spawnPos, Quaternion.identity);
         Food newFood = foodObj.GetComponent<Food>();
 
-        Vector2 moveDir = spawnPos - Vector2.zero;
-        print(moveDir + ", " + speed);
-
-        newFood.SetSpeedAndDir(speed, moveDir);
+        newFood.Throw(spawnPos, speed);
     }
 }
